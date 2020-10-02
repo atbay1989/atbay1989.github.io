fae5bce77b60f7900d51940a3b6e738a9cf89658
atbay1989 <2407639b@student.gla.ac.uk>
2020-10-02 23:34:04 +0100

    canvassing

--- script.js
+++ script.js
@@ -2,18 +2,28 @@ let draw = true;
 let x = 0;
 let y = 0;
 
-const myPics = document.getElementById('canvas');
-const context = canvas.getContext('2d');
+//const myPics = document.getElementById('canvas');
+//const canvas = canvas.getContext('2d');
 
-myPics.addEventListener('mousedown', e => {
+var canvas = document.createElement('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+canvas.style.position = 'fixed';
+canvas.style.top = 0;
+canvas.style.left = 0;
+canvas.style.zIndex = -10;
+canvas.style.backgroundColor = 'transparent';
+
+
+canvas.addEventListener('mousedown', e => {
   x = e.offsetX;
   y = e.offsetY;
   draw = true;
 });
 
-myPics.addEventListener('mousemove', e => {
+canvas.addEventListener('mousemove', e => {
   if (draw === true) {
-    drawLine(context, x, y, e.offsetX, e.offsetY);
+    drawLine(canvas, x, y, e.offsetX, e.offsetY);
     x = e.offsetX;
     y = e.offsetY;
   }
@@ -21,19 +31,19 @@ myPics.addEventListener('mousemove', e => {
 
 window.addEventListener('mouseup', e => {
   if (draw === true) {
-    drawLine(context, x, y, e.offsetX, e.offsetY);
+    drawLine(canvas, x, y, e.offsetX, e.offsetY);
     x = 0;
     y = 0;
     draw = false;
   }
 });
 
-function drawLine(context, x1, y1, x2, y2) {
-  context.beginPath();
-  context.strokeStyle = 'black';
-  context.lineWidth = 2;
-  context.moveTo(x1, y1);
-  context.lineTo(x2, y2);
-  context.stroke();
-  context.closePath();
+function drawLine(canvas, x1, y1, x2, y2) {
+  canvas.beginPath();
+  canvas.strokeStyle = 'black';
+  canvas.lineWidth = 2;
+  canvas.moveTo(x1, y1);
+  canvas.lineTo(x2, y2);
+  canvas.stroke();
+  canvas.closePath();
 }
