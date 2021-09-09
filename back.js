97e543114597f51a8192ab314e3b9cbafb5bff10
jamesmmbrown <240763b@student.gla.ac.uk>
2021-09-10 00:08:02 +0100

    Put back

--- back.js
+++ back.js
@@ -5,24 +5,28 @@ let y = 0;
 const myPics = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 
-myPics.addEventListener('mousemove', e => {
-  drawLine(context, x, y, e.offsetX, e.offsetY);
+myPics.addEventListener('mousedown', e => {
   x = e.offsetX;
   y = e.offsetY;
-  drawLine(context, x, y, e.offsetX, e.offsetY);
-  x = 0;
-  y = 0;
-  draw = false;
+  draw = true;
 });
 
-// window.addEventListener('mouseup', e => {
-//   if (draw === true) {
-//     drawLine(context, x, y, e.offsetX, e.offsetY);
-//     x = 0;
-//     y = 0;
-//     draw = false;
-//   }
-// });
+myPics.addEventListener('mousemove', e => {
+  if (draw === true) {
+    drawLine(context, x, y, e.offsetX, e.offsetY);
+    x = e.offsetX;
+    y = e.offsetY;
+  }
+});
+
+window.addEventListener('mouseup', e => {
+  if (draw === true) {
+    drawLine(context, x, y, e.offsetX, e.offsetY);
+    x = 0;
+    y = 0;
+    draw = false;
+  }
+});
 
 function drawLine(context, x1, y1, x2, y2) {
   context.beginPath();
